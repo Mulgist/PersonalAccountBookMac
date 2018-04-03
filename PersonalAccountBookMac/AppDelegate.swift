47ed37ba3a3379a312de4d2f6c72318fcf58737b
Juwon Lee <good9797@outlook.com>
2018-04-03 16:32:30 +0900

    Initial Commit

---
+++ PersonalAccountBookMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PersonalAccountBookMac
+//
+//  Created by 이주원 on 2018. 4. 3..
+//  Copyright © 2018년 이주원. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PersonalAccountBookMac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PersonalAccountBookMac
+//
+//  Created by 이주원 on 2018. 4. 3..
+//  Copyright © 2018년 이주원. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
